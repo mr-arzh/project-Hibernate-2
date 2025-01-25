1244c2c7936b1182a6110cfa4aedeecd1403120f
mr-arzh <asiris.joy@icloud.com>
2025-01-25 15:53:44 +0300

    crating entity layer and specified classes

---
+++ src/main/java/entity/Customer.java
@@ -0,0 +1,14 @@
+package entity;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+
+public class Customer {
+
+    private Long id;
+
+    private String first_name;
+    private String last_name;
+    private String email;
+
+
+}
