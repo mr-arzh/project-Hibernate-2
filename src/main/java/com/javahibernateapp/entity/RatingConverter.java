a8dca55cb416c4805507735afbab966065e4a0b2
mr-arzh <asiris.joy@icloud.com>
2025-01-29 14:50:38 +0300

    added and implemented method customerReturnInventoryToStore(). Works fine

---
+++ src/main/java/com/javahibernateapp/entity/RatingConverter.java
@@ -0,0 +1,8 @@
+package com.javahibernateapp.entity;
+
+import jakarta.persistence.AttributeConverter;
+
+ class EnumConverter implements AttributeConverter<Enum, String> {
+
+    
+}
---
+++ src/main/java/com/javahibernateapp/entity/YearAttributeCoverter.java
@@ -0,0 +1,4 @@
+package com.javahibernateapp.entity;
+
+public class YearAttributeCoverter {
+}
